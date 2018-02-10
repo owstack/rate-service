106cedd948a871b8e7fd23610df59819dc1acfc2
Ian Patton <ian@owstack.org>
2018-02-10 12:36:43 -0500

    working REST

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./lib/config');
+const config = require('./config');
 const polka = require('polka');
 const market = {
     gdax: {
@@ -22,18 +22,38 @@ const getOrderBook = (req, res) => {
         });
 };
 
-const getQuote = (req, res) => {
+const getMarketSellPrice = (req, res) => {
     const name = req.params.exchange;
     const pair = req.params.pair;
     const amount = Number(req.params.amount);
-    console.log(`requested quote for amount ${amount} from ${req.params.exchange} ${req.params.pair}`);
-    const quote = market[name][pair].quotePrice(amount);
+    console.log(`requested sell quote for amount ${amount} from ${req.params.exchange} ${req.params.pair}`);
+    const quote = market[name][pair].getMarketSellPrice(amount);
+    res.end(JSON.stringify(quote));
+};
+
+const getMarketBuyPrice = (req, res) => {
+    const name = req.params.exchange;
+    const pair = req.params.pair;
+    const amount = Number(req.params.amount);
+    console.log(`requested sell quote for amount ${amount} from ${req.params.exchange} ${req.params.pair}`);
+    const quote = market[name][pair].getMarketBuyPrice(amount);
+    res.end(JSON.stringify(quote));
+};
+
+const getConversionRate = (req, res) => {
+    const name = req.params.exchange;
+    const pair = req.params.pair;
+    const amount = Number(req.params.amount);
+    console.log(`requested conversion rate for amount ${amount} from ${req.params.exchange} ${req.params.pair}`);
+    const quote = market[name][pair].getConversionRate(amount);
     res.end(JSON.stringify(quote));
 };
 
 polka()
     .get('/orderbook/:exchange/:pair', getOrderBook)
-    .get('/quote/:exchange/:pair/:amount', getQuote)
+    .get('/quote/sell/:exchange/:pair/:amount', getMarketSellPrice)
+    .get('/quote/buy/:exchange/:pair/:amount', getMarketBuyPrice)
+    .get('/convert/:exchange/:pair/:amount', getConversionRate)
     .listen(config.get('port'))
     .then(() => {
         console.log(`Listening on port ${config.get('port')}`);
